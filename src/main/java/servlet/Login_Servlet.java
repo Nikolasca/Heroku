c246cb3b70666f5ac1c5da74962602090f8fcb9f
N�kolas Cardona <nikolasca@unisabana.edu.co>
2019-04-30 17:12:56 -0500

    prueba login2

--- src/main/java/servlet/Login_Servlet.java
+++ src/main/java/servlet/Login_Servlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
  import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(
-        name = "MyServlet", 
+        name = "ServletAcceso", 
         urlPatterns = {"/Login"}
     )
 public class Login_Servlet extends HttpServlet {
