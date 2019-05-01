7ce347dad935ff57bb04d28cd105d4312fc1c391
nikolasca <nikolasca@DESKTOP-Q817RTE>
2019-05-01 00:50:45 -0500

    usuario login 2

--- src/main/java/servlet/Login_Servlet.java
+++ src/main/java/servlet/Login_Servlet.java
@@ -24,27 +24,25 @@ public class Login_Servlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         RequestDispatcher rd;
-        ServletOutputStream out = resp.getOutputStream();
         String A = req.getParameter("uname");
         String B = req.getParameter("pass");
         ArrayList<Usuario> usuarios = s.getUsuarios();
-        boolean x= false;
+        boolean x = false;
         for (Usuario usuario : usuarios) {
             if ((usuario.getUsuario().compareTo(A) == 0) && (usuario.getPassword().compareTo(B) == 0)) {
-               x = true;
-            req.setAttribute("usuario", usuario);
-            rd = req.getRequestDispatcher("/Perfil.jsp");
-            rd.forward(req, resp);
+                x = true;
+                req.setAttribute("usuario", usuario);
+                rd = req.getRequestDispatcher("/Perfil.jsp");
+                rd.forward(req, resp);
             }
-            if(x==false){
-            out.write("Paila parce".getBytes());
-            
+            if (x == false) {
+                rd = req.getRequestDispatcher("/Registro.jsp");
+                rd.forward(req, resp);
+
             }
-            
 
         }
-        out.flush();
-        out.close();
+
     }
 
 }
